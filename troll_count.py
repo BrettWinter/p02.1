9d5ef868255ba3389cb4871d91e25222ec0ad5e8
brett winter <bwinter18@VenerableBede.schools.sunderland.gov.uk>
2016-09-23 09:47:47 +0100

    Fixed problems

--- troll_count.py
+++ troll_count.py
@@ -38,3 +38,17 @@ def run_tests():
 # Edit this function:
 def troll_count(n):
 
+    if n == 1:
+        print("one")
+
+    elif n == 2:
+        print("two")
+
+    elif 3 <= n <= 8:
+        print("many")
+
+    elif 9 <= n <= 12:
+        print("lots")
+        
+
+    
